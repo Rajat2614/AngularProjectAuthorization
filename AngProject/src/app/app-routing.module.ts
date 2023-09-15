6bde55a3e58cd7581e214c6344ba977a85a63edf
RAJAT CHAUHAN <98510021+Rajat2614@users.noreply.github.com>
2023-09-15 11:46:50 +0530

    Add files via upload

---
+++ AngProject/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { RouterModule, Routes } from "@angular/router";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { NgModule } from "@angular/core";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { AuthGuard } from "./auth/auth.guard";
+
+const routes: Routes = [
+  { path: "", component: PostListComponent },
+  { path: "create", component: PostCreateComponent, canActivate: [AuthGuard] },
+  {
+    path: "edit/:postId",
+    component: PostCreateComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: "signup", component: SignupComponent },
+  { path: "login", component: LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard],
+})
+export class AppRoutingModule {}
---
+++ AngProject/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatPaginatorModule } from "@angular/material/paginator";
+
+import { AppComponent } from "./app.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { AuthInteceptor } from "./auth/auth.interceptor";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+    LoginComponent,
+    SignupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInteceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
